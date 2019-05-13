db7f469419fcc4ada0467c2e8f2c13bc46095488
Josh Comeau <joshwcomeau@gmail.com>
2019-05-13 02:23:26 -0400

    Go back to having each geyser define its own engine

--- src/hooks/use-physics-engine.hook.js
+++ src/hooks/use-physics-engine.hook.js
@@ -1,37 +1,36 @@
 import React from 'react';
 import Matter from 'matter-js';
 
-let engine, renderer;
-
 export default function usePhysicsEngine(ref) {
-  const [hasReceivedRef, setHasReceivedRef] = React.useState(null);
+  let [engine, setEngine] = React.useState(null);
+  let [renderer, setRenderer] = React.useState(null);
+
   React.useEffect(() => {
     if (!ref || !ref.current) {
       return;
     }
 
-    if (!engine) {
-      // create an engine
-      engine = Matter.Engine.create();
+    // create an engine
+    const engine = Matter.Engine.create();
 
-      // create a renderer
-      const renderer = Matter.Render.create({
-        canvas: ref.current,
-        engine,
-        options: {
-          width: ref.current.width,
-          height: ref.current.height,
-          wireframes: false,
-          background: 'transparent',
-        },
-      });
+    // create a renderer
+    const renderer = Matter.Render.create({
+      canvas: ref.current,
+      engine,
+      options: {
+        width: ref.current.width,
+        height: ref.current.height,
+        wireframes: false,
+        background: 'transparent',
+      },
+    });
 
-      Matter.Engine.run(engine);
-      Matter.Render.run(renderer);
-    }
+    Matter.Engine.run(engine);
+    Matter.Render.run(renderer);
 
-    setHasReceivedRef(true);
+    setEngine(engine);
+    setRenderer(renderer);
   }, [ref]);
 
-  return [engine];
+  return [engine, renderer];
 }
